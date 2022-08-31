3412677e3e605eab5c1391302356a35b372fd79c
Dinne Kopelevich <dinne22@hotmail.com>
2022-08-31 11:34:00 -0600

    Add the wireframe and script starter

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,35 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-console.log(userData,"<>>>>userData")
+console.log(userData, "<>>>>userData");
 // An example of how you tell webpack to use a CSS file
-import './css/styles.css';
+import "./css/styles.css";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log("This is the JavaScript entry file - your code begins here.");
 
 // An example of how you tell webpack to use a JS file
 
-import userData from './data/users';
+import userData from "./data/users";
+import User from "./User";
 
-import UserRepository from './UserRepository';
+import UserRepository from "./UserRepository";
+
+//SELECTORS
+
+//EVENT LISTENERS
+
+//GLOBAL VARIABLES
+let currentUser;
+
+//FETCH CALLS
+
+//RANDOM USER FUNCTION TO BE PUT IN THE FETCH CALL
+currentUser = new User(userData[Math.floor(Math.random() * userData.length)]);
+console.log({ currentUser });
+
+//HELPER FUNCTIONS
+
+//FUNCTIONS
