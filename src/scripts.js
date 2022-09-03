8b6cc1da14d24e3e11a8b670b539f645f2b2ef7f
Matt Walter <matt.walter.tx@outlook.com>
2022-09-03 12:53:59 -0500

    Feature: add DOM selectors for the sleep class to scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -68,6 +68,10 @@ const weeklyHydration = document.querySelector(".week-ounces");
 //const ounceInfo = document.querySelector(".indiv-ounce");
 
 //SLEEP SELECTORS
+const sleepWidget = document.querySelector(".user-sleep-widget");
+const singleSleep = document.querySelector("single-sleep-data");
+const weeklySleep = document.querySelector("week-sleep-data");
+const allTimeAvgs = document.querySelector("all-time-sleep-avgs");
 
 //FRIEND SELECTORS
 const friendWidget = document.querySelector(".user-friends-widget");
